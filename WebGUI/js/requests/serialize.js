47a91c53d1d7f341242cb47eeecbe9e9c4e228ff
tailoric <eric.schneider96@gmail.com>
2017-12-01 21:24:38 +0100

    put title instead of indices into the cluster

--- WebGUI/js/requests/serialize.js
+++ WebGUI/js/requests/serialize.js
@@ -5,34 +5,44 @@ function place_seach_graphic(event) {
         url : "http://localhost:9200/zotero/entry/_search_with_clusters",
         /* url request hidden Data fields with id #...*/
         data : $("#search_form").serialize(),
-        success: function(data){
-            var groups_arr = [];
-            var doc_group = [];
-            data.clusters.forEach(function(cluster){
-                cluster.documents.forEach(function(document_obj){
-                    var obj = {
-                        label: document_obj,
-                        weight : 2
-                    };
-                    doc_group.push(obj);
-                });
-                var cluster_obj = {
-                    label: cluster.label,
-                    weight: cluster.score,
-                    groups: doc_group
-                }
-                doc_group = [];
-                groups_arr.push(cluster_obj);
+        success: create_foam_tree
+    });
+}
+function create_foam_tree(cluster_data){
+    var groups_arr = [];
+    var doc_group = [];
+    cluster_data.clusters.forEach(function(cluster){
+        cluster.documents.forEach(function(document_obj){
+            var obj = {
+                label: find_responding_document(cluster_data,document_obj),
+                weight : 2
+            };
+            doc_group.push(obj);
+        });
+        var cluster_obj = {
+            label: cluster.label,
+            weight: cluster.score,
+            groups: doc_group
+        }
+        doc_group = [];
+        groups_arr.push(cluster_obj);
 
-            });
-            foamtree.set({
-                id: "foamtree-area",
-                dataObject:{
-                    groups: groups_arr
-                }
-            });
+    });
+    foamtree.set({
+        id: "foamtree-area",
+        dataObject:{
+            groups: groups_arr
         }
     });
+}
+function find_responding_document(server_response,document_id){
+    var return_value;
+    server_response.hits.hits.forEach(function(hit){
+            if(document_id == hit._source.id){
+               return_value = hit._source.title;
+            }
+    });
+    return return_value;
 }
 $(document).ready(function(){
     foamtree = new CarrotSearchFoamTree({
@@ -40,3 +50,4 @@ $(document).ready(function(){
     });
 
 });
+
