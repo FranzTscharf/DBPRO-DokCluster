991af9c20b50a35c8421cdfc2d1aa347a7fb87b7
tailoric <eric.schneider96@gmail.com>
2018-01-18 20:23:43 +0100

    searching now changes all Kibana visualizations
    On page /visualizations all kibana visualizations will be refreshed with
    search string.
    
    When there is an error with elasticsearch then there will be now an
    error message inside the #foamtree div
    
    Also add escape for ' character while searching the Kibana
    visualizations because this breaks the iframe source link

--- DokClusterWebGUI/src/main/resources/static/js/requests/serialize.js
+++ DokClusterWebGUI/src/main/resources/static/js/requests/serialize.js
@@ -4,32 +4,45 @@
  */
 
 $(document).ready(function(){
-    foamtree = new CarrotSearchFoamTree({
-        id: "foamtree-area",
-        layout: "squarified",
-        groupMinDiameter: 0,
-        relaxationInitializer: "ordered",
-        groupGrowingEasing: "quadInOut",
-        groupBorderRadius: 0,
-        groupSelectionOutlineShadowColor: "hsla(295, 14%, 97%, 1)",
-        groupFillType: "plain",
-        groupStrokePlainLightnessShift: -20,
-        rainbowStartColor: "hsla(140, 77%, 29%, 1)",
-        rainbowEndColor: "hsla(252, 24%, 51%, 1)",
-        rolloutEasing: "squareInOut",
-        rolloutScalingStrength: -0.3,
-        rolloutRotationStrength: 0,
-        pullbackEasing: "squareInOut",
-        pullbackDuration: 2000,
-        pullbackScalingStrength: -0.3,
-        pullbackRotationStrength: 0,
-        pullbackPolygonDelay: 0.1,
-        attributionPosition: 45
-    });
+    if($("#foamtree-area").length){
+        foamtree = new CarrotSearchFoamTree({
+            id: "foamtree-area",
+            layout: "squarified",
+            groupMinDiameter: 0,
+            relaxationInitializer: "ordered",
+            groupGrowingEasing: "quadInOut",
+            groupBorderRadius: 0,
+            groupSelectionOutlineShadowColor: "hsla(295, 14%, 97%, 1)",
+            groupFillType: "plain",
+            groupStrokePlainLightnessShift: -20,
+            rainbowStartColor: "hsla(140, 77%, 29%, 1)",
+            rainbowEndColor: "hsla(252, 24%, 51%, 1)",
+            rolloutEasing: "squareInOut",
+            rolloutScalingStrength: -0.3,
+            rolloutRotationStrength: 0,
+            pullbackEasing: "squareInOut",
+            pullbackDuration: 2000,
+            pullbackScalingStrength: -0.3,
+            pullbackRotationStrength: 0,
+            pullbackPolygonDelay: 0.1,
+            attributionPosition: 45
+        });
+    }
 });
+function clusterDataAjaxCall(data){
+        return $.ajax({
+        method: "GET",
+        url : "http://localhost:9200/zotero/entry/_search_with_clusters",
+        data : data
+    });
+}
 function search_textfield_input(search_str) {
-    set_search_graphic(search_str);
-    set_kibana_graphic(search_str);
+    if($("#foamtree-area").length){
+        set_search_graphic(search_str);
+    }
+    if($(".kibana-visualization").length){
+        set_kibana_graphic(search_str);
+    }
 }
 function set_search_graphic(search_str) {
     var data = {
@@ -38,15 +51,21 @@ function set_search_graphic(search_str) {
         field_mapping_content : "_source.abstract",
         size : "100"
     };
-    $.ajax({
-        method: "GET",
-        url : "http://localhost:9200/zotero/entry/_search_with_clusters",
-        /* url request hidden Data fields with id #...*/
-        data : data,
-        success: create_foam_tree
-    });
+    var clusterAjaxCall = clusterDataAjaxCall(data);
+    clusterAjaxCall.done(create_foam_tree);
+    clusterAjaxCall.fail(show_error_message);
+}
+
+function show_error_message() {
+   var foamtreeArea = $("#foamtree-area");
+   foamtreeArea.empty()
+   var errorHeading = $("<h1>Something went wrong!</h1>").addClass("alert-heading");
+   foamtreeArea.css({"height":"360px","display":"flex","justify-content":"center","align-items":"center"});
+   foamtreeArea.addClass("alert alert-danger");
+   foamtreeArea.append(errorHeading);
 }
 function create_foam_tree(cluster_data){
+    console.log(cluster_data);
     var groups_arr = [];
     var doc_group = [];
     cluster_data.clusters.forEach(function(cluster){
@@ -63,7 +82,7 @@ function create_foam_tree(cluster_data){
             label: cluster.label,
             weight: cluster.score,
             groups: doc_group
-        }
+        };
         doc_group = [];
         groups_arr.push(cluster_obj);
 
@@ -109,9 +128,13 @@ function get_document_metadata(entryId){
 
 function set_kibana_graphic(search_str) {
 
-    var iframe_src = $("#kibanaOverviewiFrame").attr("src");
-    console.log(iframe_src);
-    var new_iframe_src = iframe_src.replace(/query:'[\w*]+'/, "query:'"+search_str+"'");
-    console.log(new_iframe_src);
-    $('#kibanaOverviewiFrame').attr("src", new_iframe_src);
+    console.log("before:",search_str);
+    search_str = search_str.replace(/'/g,'');
+    console.log("after:",search_str)
+    $(".kibana-visualization").each(function (index, element) {
+        console.log("element:", element);
+        var iframe_src = element.src;
+        var new_iframe_src = iframe_src.replace(/query:'.+?'/, "query:'"+search_str+"'");
+        element.src= new_iframe_src;
+    });
 }
