383e17ef17c17e93d36ea9d097ae5609f9ba7c56
tailoric <eric.schneider96@gmail.com>
2018-01-19 19:24:57 +0100

    housekeeping and error handling for empty response

--- DokClusterWebGUI/src/main/resources/static/js/requests/serialize.js
+++ DokClusterWebGUI/src/main/resources/static/js/requests/serialize.js
@@ -1,9 +1,4 @@
-/**
- * Dokument Loads
- *
- */
-
-$(document).ready(function(){
+function initializeFoamtree() {
     if($("#foamtree-area").length){
         foamtree = new CarrotSearchFoamTree({
             id: "foamtree-area",
@@ -28,14 +23,32 @@ $(document).ready(function(){
             attributionPosition: 45
         });
     }
+}
+/**
+ * function that triggers when the page finished loading,
+ * also initializes the foamtree area for carrot visualization
+ */
+$(document).ready(function(){
+    initializeFoamtree();
 });
-function clusterDataAjaxCall(data){
+
+/**
+ * creates a call to the elasticsearch instance running in the background for returning cluster
+ * @param data a plain object with parameters for the endpoint call
+ * @returns an ajax object for further usage with promises
+ */
+function cluster_data_ajax_call(data){
         return $.ajax({
         method: "GET",
         url : "http://localhost:9200/zotero/entry/_search_with_clusters",
         data : data
     });
 }
+
+/**
+ * function for forwarding search requests to the foamtree visualization and the kibana visualization
+ * @param search_str the text string from the search field
+ */
 function search_textfield_input(search_str) {
     if($("#foamtree-area").length){
         set_search_graphic(search_str);
@@ -44,6 +57,11 @@ function search_textfield_input(search_str) {
         set_kibana_graphic(search_str);
     }
 }
+
+/**
+ * makes an ajax call to the cluster endpoint of the elasticsearch endpoint and handles the response
+ * @param search_str the text string form the search field.
+ */
 function set_search_graphic(search_str) {
     var data = {
         q : search_str,
@@ -51,21 +69,33 @@ function set_search_graphic(search_str) {
         field_mapping_content : "_source.abstract",
         size : "100"
     };
-    var clusterAjaxCall = clusterDataAjaxCall(data);
+    var clusterAjaxCall = cluster_data_ajax_call(data);
     clusterAjaxCall.done(create_foam_tree);
     clusterAjaxCall.fail(show_error_message);
 }
 
+/**
+ * creates a simple error message in the foamtree visualization div on error from the elasticsearch endpoint
+ */
 function show_error_message() {
-   var foamtreeArea = $("#foamtree-area");
-   foamtreeArea.empty()
-   var errorHeading = $("<h1>Something went wrong!</h1>").addClass("alert-heading");
-   foamtreeArea.css({"height":"360px","display":"flex","justify-content":"center","align-items":"center"});
-   foamtreeArea.addClass("alert alert-danger");
-   foamtreeArea.append(errorHeading);
+    foamtree = null;
+    var foamtreeArea = $("#foamtree-area");
+    foamtreeArea.empty();
+    var errorHeading = $("<h1>Something went wrong!</h1>").addClass("alert-heading");
+    foamtreeArea.css({"height":"360px","display":"flex","justify-content":"center","align-items":"center"});
+    foamtreeArea.addClass("alert alert-danger");
+    foamtreeArea.append(errorHeading);
 }
+
+/**
+ * sets the foamtree and visualizes the cluster.
+ * @param cluster_data the data of the cluster
+ */
 function create_foam_tree(cluster_data){
-    console.log(cluster_data);
+    if (cluster_data.hits.total === 0){
+        alert("empty response");
+        return;
+    }
     var groups_arr = [];
     var doc_group = [];
     cluster_data.clusters.forEach(function(cluster){
@@ -87,18 +117,27 @@ function create_foam_tree(cluster_data){
         groups_arr.push(cluster_obj);
 
     });
-    foamtree.set({
-        id: "foamtree-area",
-        dataObject:{
-            groups: groups_arr
-        },
-        onGroupDoubleClick: function(event){
-            if(event.group.isEntry){
-                get_document_metadata(event.group.entryId);
+    if(typeof foamtree !== "undefined"){
+        foamtree.set({
+            id: "foamtree-area",
+            dataObject:{
+                groups: groups_arr
+            },
+            onGroupDoubleClick: function(event){
+                if(event.group.isEntry){
+                    get_document_metadata(event.group.entryId);
+                }
             }
-        }
-    });
+        });
+    }
 }
+
+/**
+ * finds the corresponding document to the document id to assign the document title to the document
+ * @param server_response the cluster response from elasticsearch
+ * @param document_id the id of the document
+ * @returns the title of the document
+ */
 function find_responding_document(server_response,document_id){
     var return_value;
     server_response.hits.hits.forEach(function(hit){
@@ -109,32 +148,47 @@ function find_responding_document(server_response,document_id){
     return return_value;
 }
 
-function get_document_metadata(entryId){
-    $.ajax({
+/**
+ * create a ajax call to get the metadata of an entry in the elasticsearch database
+ * @param entryId the id of the document.
+ */
+function create_meta_data_ajax_call(entryId){
+    return $.ajax({
         method: "GET",
-        url : "http://localhost:9200/zotero/entry/"+entryId,
-        /* url request hidden Data fields with id #...*/
-        success: function(data){
-            $("#metadata_table").empty();
-            $("#metadata_table").append('<tr><th>Key</th><th>Value</th></tr>')
-            $.each(data._source, function(key, value){
-                $("#metadata_table").append(
-                    "<tr><td style='width: 20%'>"+key+"</td><td>"+value+"</td></tr>"
-                )
-            });
-        }
-    })
+        url : "http://localhost:9200/zotero/entry/"+entryId
+    });
 }
 
-function set_kibana_graphic(search_str) {
+/**
+ * appends the table with id #metadata-table with the metadata of the document that was clicked inside the visualization
+ * @param data the server response with the metadata about the document.
+ */
+function append_metada_table(data) {
+    $("#metadata_table").empty();
+    $("#metadata_table").append('<tr><th>Key</th>th><th>Value</th>th></tr>tr>');
+    $.each(data._source, function (key, value) {
+        $("#metadata_table").append(
+            "<tr><td style=''width: 20%'>" + key + "</td><td>" + value + "</td></tr>tr>"
+        )
+    });
+}
+/**
+ * makes an Ajax call and then appends the information to a table.
+ * @param entryId the id of the document that was clicked in the foamtree graphic.
+ */
+function get_document_metadata(entryId){
+    var ajaxCall = create_meta_data_ajax_call(entryId);
+    ajaxCall.done(append_metada_table);
+}
 
-    console.log("before:",search_str);
-    search_str = search_str.replace(/'/g,'');
-    console.log("after:",search_str)
+/**
+ * changes the source link of the kibana visualization's iframe to include the results from the search
+ * @param search_str the text searched by the user from the form
+ */
+function set_kibana_graphic(search_str) {
+    search_str = search_str.replace(/'/g,''); // basically escape all ' symbols
     $(".kibana-visualization").each(function (index, element) {
-        console.log("element:", element);
         var iframe_src = element.src;
-        var new_iframe_src = iframe_src.replace(/query:'.+?'/, "query:'"+search_str+"'");
-        element.src= new_iframe_src;
+        element.src= iframe_src.replace(/query:'.+?'/, "query:'" + search_str + "'");
     });
 }
